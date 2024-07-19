9474990f173c8aafc26619927b71855820cdfad9
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-07-20 01:32:15 +0530

    change route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 
 const port = process.env.PORT;
 app.listen(port,
