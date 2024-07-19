7d0c80502c5a513feaf0efea4d98c6f76a962b62
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-07-20 00:26:43 +0530

    connect to mongodb Atlas

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db'); 
 const apiRoutes = require('./routes'); 
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,4 @@ app.set('view engine', 'ejs');
 app.use('/api', apiRoutes);
 
 
-app.listen(5000, () => {
-  console.log(`Server is running on port http://localhost:5000/api`);
-});
+app.listen();
--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
