9b59dfdf5bd9d25688f519c8d35340ad215cd17f
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-07-25 11:40:12 +0530

    changes routes

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use('/', apiRoutes);
+app.use('/api', apiRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port,
-console.log(`http://localhost:${port}/`)
+console.log(`http://localhost:${port}/api/`)
 );
--- db.js
+++ db.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
